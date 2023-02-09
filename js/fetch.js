16db062c421b0024a2b46dac6a7fcd9f5f948ed4
BManta <novoarodriguezmiguel@gmail.com>
2023-02-09 10:59:15 +0100

    Ajout bulma.css

--- js/fetch.js
+++ js/fetch.js
@@ -4,17 +4,30 @@ $(document).ready((e)=>{
         $.getJSON("js/cars.json", function(data){
             let carsList = []
             $.each(data, function(){
-                carsList.push(`<ul id='${this.id_voitures}'
-                    <li>Marque : ${this.marque}</li>
-                    <li>Annee : ${this.annee}</li>
-                    <li>Type : ${this.type}</li>
-                    <li>Etat : ${this.etat}</li>
-                    <li>${this.description}</li>
-                    <img src='/sources${this.image}' alt='image ${this.marque}'>
-                </ul>`)
-            })
-            $('.carsBox').append(carsList)
-            console.log(data)
+                carsList.push(`
+                    <div id='${this.id_voitures}' role="region" class="card">
+                        <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src='/sources${this.image}' alt='image ${this.marque}'>
+                        </figure>
+                        </div>
+                        <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                            <p class="title is-4">${this.marque} | ${this.annee}</p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            ${this.description}
+                            <br>
+                            <p>Type : ${this.type} - Etat : ${this.etat}</p>
+                        </div>
+                        </div>
+                    </div>`
+                );
+            });
+            $('.carsBox').append(carsList);
+            console.log(data);
         })
     })
 });
