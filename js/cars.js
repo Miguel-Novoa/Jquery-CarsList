71cd3b288b2f80a134b308d81ced9f22c76e1e79
BManta <novoarodriguezmiguel@gmail.com>
2023-02-09 10:08:15 +0100

    first commit

---
+++ js/cars.js
@@ -0,0 +1,29 @@
+[
+    {
+       "id_voitures":"1",
+       "marque":"Fiat",
+       "annee":"1960",
+       "type":"Utilitaire",
+       "etat":"Ancien",
+       "description":"Utilis\u00e9e souvent dans les films anciens comme \"La dolce vita\"",
+       "image":"/1950-500.jpg"
+    },
+    {
+       "id_voitures":"2",
+       "marque":"Ford-Mustang",
+       "annee":"1977",
+       "type":"Sport",
+       "etat":"Ancien",
+       "description":"Utilis\u00e9e dans des films d'action am\u00e9ricain",
+       "image":"/ford.jpg"
+    },
+    {
+       "id_voitures":"3",
+       "marque":"DeLorean",
+       "annee":"1982",
+       "type":"Sport",
+       "etat":"Ancien",
+       "description":"\"Retour vers le futur\" : DeLorean, une histoire nord-irlandaise ",
+       "image":"/delorean.jpg"
+    }
+]
---
+++ js/fetch.js
@@ -0,0 +1,20 @@
+$(document).ready((e)=>{
+    e.preventDefault;
+    $('.displayBtn').on('click', function(){
+        $.getJSON("js/cars.js", function(data){
+            let carsList = []
+            $.each(data, function(){
+                carsList.push(`<ul id='${this.id_voitures}'
+                <li>Marque : ${this.marque}</li>
+                <li>Annee : ${this.annee}</li>
+                <li>Type : ${this.type}</li>
+                <li>Etat : ${this.etat}</li>
+                <li>${this.description}</li>
+                <img src='/sources${this.image}' alt='image ${this.marque}'>
+                </ul>`)
+            })
+            $('.carsBox').append(carsList)
+            console.log(data)
+        })
+    })
+});
