849b5b0993a8101cc6318604b0de1072fc9c81de
BManta <novoarodriguezmiguel@gmail.com>
2023-02-10 14:55:27 +0100

    refonte css

--- js/fetch.js
+++ js/fetch.js
@@ -1,28 +1,19 @@
 $(document).ready((e)=>{
     e.preventDefault;
-    $('.displayBtn').on('click', function(){
+    $('.displayBtn').one('click', function(){
         $.getJSON("js/cars.json", function(data){
             let carsList = []
             $.each(data, function(){
                 carsList.push(`
-                    <div id='${this.id_voitures}' role="region" class="card">
-                        <div class="card-image">
-                        <figure class="image is-4by3">
+                    <div id='${this.id_voitures}' role="group" class="card">
+                        <figure class="image is-4by4">
                             <img src='/sources${this.image}' alt='image ${this.marque}'>
+                            <figcaption>
+                                <h3 class="title is-4">${this.marque}   |   ${this.annee}</h3>
+                                <p>${this.description}</p>
+                                <p>Type : ${this.type} - Etat : ${this.etat}</p>
+                            </figcaption>
                         </figure>
-                        </div>
-                        <div class="card-content">
-                        <div class="media">
-                            <div class="media-content">
-                            <p class="title is-4">${this.marque} | ${this.annee}</p>
-                            </div>
-                        </div>
-                        <div class="content">
-                            ${this.description}
-                            <br>
-                            <p>Type : ${this.type} - Etat : ${this.etat}</p>
-                        </div>
-                        </div>
                     </div>`
                 );
             });
